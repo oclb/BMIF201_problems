2d93d8695d17cee3ebe8cf0e9f9831f8f27b7075
Luke Jen O'Connor <loconnor@broadinstitute.org>
2024-09-29 11:13:54 -0400

    Lecture 2 pset

---
+++ lecture02_wright_fisher.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+class WrightFisher:
+    mutation_rate: float
+    population_size: int
+    selection_coefficient: float
+
+    # This is the function that is called when you write: model = WrightFisher(...)
+    def __init__(self, mutation_rate: float, population_size: int, selection_coefficient: float = 0):
+        self.mutation_rate = mutation_rate
+        self.population_size = population_size # N, not 2N
+        self.selection_coefficient = selection_coefficient
+
+    def next_generation(self, allele_frequencies: np.ndarray) -> np.ndarray:
+        """
+        :param allele_frequencies: array of allele frequencies in generation n
+        :return: array of allele frequencies in generation n+1
+        """
+
+        expected_frequency = allele_frequencies.copy()  # avoid modifying input array
+
+        # Effect of selection
+        expected_frequency = expected_frequency # TODO
+
+        # Effect of mutation
+        expected_frequency = (expected_frequency * (1 - self.mutation_rate) +
+                              (1 - expected_frequency) * self.mutation_rate)
+
+        # Effect of drift
+        binomial_samples = np.random.binomial(2 * self.population_size, expected_frequency)
+
+        return binomial_samples / (2 * self.population_size)
+
+    def until_fixation(self, initial_allele_frequencies: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        :param initial_allele_frequencies: initial state
+        :return: final allele frequencies (0/1-valued), time to fixation or loss
+        """
+        if self.mutation_rate > 0:
+            raise ValueError("Mutation rate should be zero to analyze fixation")
+
+        allele_frequencies = initial_allele_frequencies.copy()  # avoids modifying input array
+        variants = np.where(allele_frequencies * (1 - allele_frequencies) > 0)[0]  # sites not yet fixed or lost
+        time_to_fixation_or_loss = np.zeros_like(allele_frequencies, dtype=int)
+
+        generations = 0
+        while len(variants) > 0:
+            generations += 1
+            allele_frequencies[variants] = self.next_generation(allele_frequencies[variants])
+            newly_fixed_variants = (allele_frequencies[variants] * (1 - allele_frequencies[variants]) == 0)
+            time_to_fixation_or_loss[variants[newly_fixed_variants]] = generations
+            variants = variants[~newly_fixed_variants]
+
+        assert np.all(allele_frequencies * (1 - allele_frequencies) == 0)
+
+        return allele_frequencies, time_to_fixation_or_loss
+
